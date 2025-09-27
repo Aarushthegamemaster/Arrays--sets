9d2bb8a6a164e2ab5bcfdd10b55185c3467e0a55
Aarushthegamemaster <aarushchandra0604@gmail.com>
2025-09-26 18:24:43 -0700

    Sets_Are_Cool

---
+++ Code2.py
@@ -0,0 +1,8 @@
+setx = {"green", "blue"}
+sety = {"yellow", "blue"}
+print("The original sets are:")
+print(setx)
+print(sety)
+setz = setx.intersection(sety)
+print("The new sets are:")
+print(setz)
