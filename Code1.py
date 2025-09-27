cb74b3e5342bd7d5da9c3fe51b5e922c2adc22eb
Aarushthegamemaster <aarushchandra0604@gmail.com>
2025-09-26 18:17:00 -0700

    Sets_Are_Cool

---
+++ Code1.py
@@ -0,0 +1,13 @@
+set1 = {1,2,3}
+print(set1)
+
+set2 = {1.0, "Hello World", (1,2,3)}
+print(set2)
+
+set3 = {1,2,3,4,2}
+print(set3)
+
+set4 = {1,2,3,4,5,6}
+print("Orginal set:", set4)
+set4.pop()
+print("New set:", set4)
